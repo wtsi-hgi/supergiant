87de7d76a44359ee97ca6e0478aa194f65ecd0e8
Ben Hundley <ben.hundley@gmail.com>
2016-05-04 11:04:47 -0700

    Recover from panic in Supervisor
    While recovering from a panic in the background makes spotting bugs a
    little trickier--mainly because we're missing backtrace (which I assume
    could be added)--it prevents lingering RUNNING tasks after panics, which
    make using the API very difficult.

---
+++ hack/deploy_jenkins.sh
@@ -0,0 +1,38 @@
+curl -XPOST localhost:8080/v0/entrypoints -d '{
+  "domain": "example.com"
+}' || true
+
+curl -XPOST localhost:8080/v0/apps -d '{
+  "name": "jenkins"
+}'
+
+curl -XPOST localhost:8080/v0/apps/jenkins/components -d '{
+  "name": "jenkins"
+}'
+
+curl -XPOST localhost:8080/v0/apps/jenkins/components/jenkins/releases -d '{
+  "containers": [
+    {
+      "image": "jenkins",
+      "cpu": {
+        "min": 0,
+        "max": 0
+      },
+      "ram": {
+        "min": 0,
+        "max": 0
+      },
+      "ports": [
+        {
+          "protocol": "HTTP",
+          "number": 8080,
+          "external_number": 33666,
+          "public": true,
+          "entrypoint_domain": "example.com"
+        }
+      ]
+    }
+  ]
+}'
+
+curl -XPOST localhost:8080/v0/apps/jenkins/components/jenkins/deploy
