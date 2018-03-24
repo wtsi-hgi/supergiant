0180c5025bfaa270506b2c96d7ef6cbb260fd578
gopherstein <spankenstien@gmail.com>
2018-03-24 16:34:50 -0500

    General Stability Update
    This change cleans up some incorrect variable scope,
    and reference to context menu methods that do not exist.
    This was causing some performance impact to the UI application.

--- cmd/ui/assets/src/app/apps/apps.service.ts
+++ cmd/ui/assets/src/app/apps/apps.service.ts
@@ -4,4 +4,26 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AppsService {
+  selectedItems = new Array();
+
+  isChecked(item) {
+    for (const obj of this.selectedItems) {
+      if (item.id === obj.id) { return true; }
+    }
+    return false;
+  }
+
+  selectItem(item, event) {
+    if (event) {
+      this.selectedItems.push(item);
+    } else {
+      for (const obj of this.selectedItems) {
+        if (item.id === obj.id) {
+          this.selectedItems.splice(
+            this.selectedItems.indexOf(obj), 1);
+        }
+      }
+    }
+  }
+
 }
